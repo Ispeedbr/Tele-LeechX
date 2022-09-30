4d39301166386437166245cc8b690e4060bdb39d
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-30 22:03:42 +0530

    Update help_func.py

--- tobrot/plugins/help_func.py
+++ tobrot/plugins/help_func.py
@@ -204,7 +204,7 @@ async def settings_callback(client, query: CallbackQuery):
 ┃
 ┣ <b>Log Channel Leech :</b> {'Enabled' if __log_id else 'Disabled'}
 ┗ <b>Log Channel ID :</b> {__log_id or '-'}'''
-        await query.edit_message_caption(caption=_text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Disable Log Channel', callback_data=f'setlogdis {usid}')][InlineKeyboardButton("⌫ Back", callback_data = f"sethome {usid}")]]))
+        await query.edit_message_caption(caption=_text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Disable Log Channel', callback_data=f'setlogdis {usid}')], [InlineKeyboardButton("⌫ Back", callback_data = f"sethome {usid}")]]))
     elif query.data.startswith("sethome"):
         lcode = query.from_user.language_code
         did = query.from_user.dc_id
