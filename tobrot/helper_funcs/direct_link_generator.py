ef0a43336c5bcc6f78b7a1d7962e65f8687c30e1
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-10-01 22:39:47 +0530

    Update direct_link_generator.py

--- tobrot/helper_funcs/direct_link_generator.py
+++ tobrot/helper_funcs/direct_link_generator.py
@@ -9,6 +9,7 @@
 
 
 import json
+import hashlib
 import re
 import urllib.parse
 import lk21
@@ -147,6 +148,8 @@ def url_link_generate(text_url: str):
         return wetransfer(text_url)
     elif "corneey.com" in text_url or "sh.st" in text_url:
         return shorte_st(text_url)
+    elif "psa.pm" in text_url:
+        return psa_bypasser(text_url)
     else:
         raise DirectDownloadLinkException(f'UnSupported URL : {text_url}')
         ### Direct Links Generator ----
@@ -1324,3 +1327,44 @@ def gadrive_dl(url):
     info_parsed['src_url'] = url
 
     return info_parsed
+
+def try2link_bypass(url):
+	client = cloudscraper.create_scraper(allow_brotli=False)
+	
+	url = url[:-1] if url[-1] == '/' else url
+	
+	params = (('d', int(time.time()) + (60 * 4)),)
+	r = client.get(url, params=params, headers= {'Referer': 'https://newforex.online/'})
+	
+	soup = BeautifulSoup(r.text, 'html.parser')
+	inputs = soup.find(id="go-link").find_all(name="input")
+	data = { input.get('name'): input.get('value') for input in inputs }	
+	time.sleep(7)
+	
+	headers = {'Host': 'try2link.com', 'X-Requested-With': 'XMLHttpRequest', 'Origin': 'https://try2link.com', 'Referer': url}
+	
+	bypassed_url = client.post('https://try2link.com/links/go', headers=headers,data=data)
+	return bypassed_url.json()["url"]
+		
+
+def try2link_scrape(url):
+	client = cloudscraper.create_scraper(allow_brotli=False)	
+	h = {
+	'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+	}
+	res = client.get(url, cookies={}, headers=h)
+	url = 'https://try2link.com/'+re.findall('try2link\.com\/(.*?) ', res.text)[0]
+	return try2link_bypass(url)
+    
+
+def psa_bypasser(psa_url):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    r = client.get(psa_url)
+    soup = BeautifulSoup(r.text, "html.parser").find_all(class_="dropshadowboxes-drop-shadow dropshadowboxes-rounded-corners dropshadowboxes-inside-and-outside-shadow dropshadowboxes-lifted-both dropshadowboxes-effect-default")
+    links = ""
+    for link in soup:
+        try:
+            exit_gate = link.a.get("href")
+            links = links + try2link_scrape(exit_gate) + '\n'
+        except: pass
+    return links
